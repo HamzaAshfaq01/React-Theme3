1a81bd4e5d42a38f3284e97848e7415e4e10ca36
Hamza Ashfaq <hamzaashfaq7866@gmail.com>
2020-08-30 14:00:51 +0500

    Updates

---
+++ precache-manifest.507d1634ad3148f49c2e4e69a06c1178.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f079ccea2fb2afa428e0d52e05d4405",
+    "url": "/React-Theme3/index.html"
+  },
+  {
+    "revision": "a901a7ee6e49fea6e987",
+    "url": "/React-Theme3/static/css/main.2b0e5440.chunk.css"
+  },
+  {
+    "revision": "19ea5866dc2fab8d6199",
+    "url": "/React-Theme3/static/js/2.ce05fc45.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Theme3/static/js/2.ce05fc45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a901a7ee6e49fea6e987",
+    "url": "/React-Theme3/static/js/main.7f06d0be.chunk.js"
+  },
+  {
+    "revision": "87b27bcd83ca66a3432f",
+    "url": "/React-Theme3/static/js/runtime-main.366e04a6.js"
+  },
+  {
+    "revision": "567a5962ae50272ff3dc1a53bb06f49a",
+    "url": "/React-Theme3/static/media/Slider-background-v4.567a5962.png"
+  },
+  {
+    "revision": "e77efd218ac664bbf996270170902c44",
+    "url": "/React-Theme3/static/media/footer-bg.e77efd21.jpg"
+  },
+  {
+    "revision": "ec8b30dcb4c0f730f22b5c307a4ba1aa",
+    "url": "/React-Theme3/static/media/portfolio-freelance.ec8b30dc.jpg"
+  },
+  {
+    "revision": "3923df80579b871b902cbbf78dddcae6",
+    "url": "/React-Theme3/static/media/screenshot-jevelin-startup-clean.3923df80.jpg"
+  }
+]);
